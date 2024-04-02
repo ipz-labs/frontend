39cfa9daecea7cbdefec160550026013a0bab30b
acherkasoov <andreyc4060@gmail.com>
2024-04-02 11:11:01 +0300

    develop front

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { router } from './routes/router'
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#48BDE2',
+		},
+		secondary: {
+			main: '#FFBF5B',
+		},
+		dark: {
+			main: '#292A2D',
+			contrastText: '#fff',
+		},
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					fontFamily: "'Rubik', sans-serif",
+					fontWeight: '300',
+					borderRadius: '100px'
+				},
+			},
+		},
+	},
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<ThemeProvider theme={theme}>
+		<RouterProvider router={router} />
+	</ThemeProvider>
+)
